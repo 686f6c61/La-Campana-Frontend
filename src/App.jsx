679b3cf6175f65c584a73225f92ad99715f3fb35
Yesika Perez Ravelo <yesikapr@gmail.com>
2024-12-12 15:55:37 -0300

    defined react router dom and modified home

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
-import './App.css'
-import TopBar from './components/layouts/TopBar';
-import Header from './components/common/Header';
-import Banner from './components/common/Banner';
-import Slider from './components/layouts/Slider';
-import ProductSection from './components/layouts/ProductSection';
-import Footer from './components/layouts/Footer';
-import Navbar from './components/common/NavBar';
-import Feature from './components/common/Feature';
-import Sales from './components/common/Sales'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/pages/Home/Home";
 
 function App() {
   return (
-    <>
-      <div className="font-sans">
-        <TopBar />
-        <Header />
-        <Navbar />
-        <Banner />
-        <Slider />
-        <Feature />
-        <Sales />
-        <ProductSection />
-        <Footer />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home greeting={"Bienvenidos"} />} />
 
-      </div>
-    </>
-  )
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
