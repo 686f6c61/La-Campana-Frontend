04dc89cdfd749647022a169c2d16292614ed01d6
Yesika Perez Ravelo <yesikapr@gmail.com>
2024-12-13 12:54:08 -0300

    resolve team issues

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import TopBar from "../../../components/layouts/TopBar";
-
 import Header from "../../../components/common/Header";
 import Banner from "../../../components/common/Banner";
 import Slider from "../../../components/layouts/Slider";
 import ProductSection from "../../../components/layouts/ProductSection";
 import Footer from "../../../components/layouts/Footer";
 import Navbar from "../../../components/common/NavBar";
-import Feature from "../../../components/common/Feature";
-import Sales from "../../../components/common/Sales";
+import TrustSection from "../../../components/common/TrustSection";
+import BestofLaCampana from "../../../components/common/BestofLaCampana";
 
 const Home = () => {
   return (
@@ -18,8 +17,8 @@ const Home = () => {
       <Navbar />
       <Banner />
       <Slider />
-      <Feature />
-      <Sales />
+      <TrustSection />
+      <BestofLaCampana />
       <ProductSection />
       <Footer />
     </div>
