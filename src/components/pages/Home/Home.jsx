e8915919dc25f75896d151b7e94db3b83717f12f
Antonella Quintana <antonellaq25@gmail.com>
2025-01-09 16:39:52 -0300

    with '#' will be ignored, and an empty message aborts the commit.

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import TopBar from "../../../components/layouts/TopBar";
 import Header from "../../../components/common/Header";
 import Banner from "../../../components/common/Banner";
 import Slider from "../../../components/layouts/Slider";
-import ProductSection from "../../../components/layouts/ProductSection";
 import Footer from "../../../components/layouts/Footer";
 import Navbar from "../../../components/common/NavBar";
 import TrustSection from "../../../components/common/TrustSection";
@@ -26,7 +25,6 @@ const Home = () => {
       <BestofLaCampana />
       <Categories />
       <News />
-      <ProductSection />
       <FloatingDrawer />
       <ServiceSection />
       <Footer />
