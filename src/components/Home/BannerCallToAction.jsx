7b11a28c2690c5841b3c73d4cc748ffe83f983e1
AkiraYago <the.programmer.yago@gmail.com>
2025-02-04 14:03:38 -0300

    update: add action button in banner section

--- src/components/Home/BannerCallToAction.jsx
+++ src/components/Home/BannerCallToAction.jsx
@@ -1,8 +1,9 @@
 import BannerDot from "./BannerDot"
+import ActionButton from "../common/ActionButton"
 
 const BannerCallToAction = () => {
   return (
-    <div className="absolute bottom-0 bg-white w-4/5 tablet:w-2/5 p-6 text-start rounded-se-xl flex flex-col gap-2">
+    <div className="absolute bottom-0 bg-white w-4/5 tablet:w-2/5 p-6 text-start rounded-se-xl flex flex-col gap-4">
       <p className="font-open-sans text-sub-h1 lg:text-sub-h1-desktop text-lacampana-red1 leading-tight">
         SOMOS LA EMPRESA LÍDER EN
       </p>
@@ -12,7 +13,10 @@ const BannerCallToAction = () => {
           Lorem ipsum dolor sit amet consectetur. Nunc est fermentum dis arcu odio. Vel eu habitant sit duis amet nisl nulla vel. Blandit duis at quis amet cursus diam integer.
         </p>
       </div>
-      <a href="#" className="lacampana-btn">Comprar ahora</a>
+      <ActionButton 
+        text="Comprar ahora"
+        link="/products"
+      />
       <div className="w-full flex justify-end items-center gap-2 py-2 min-h-12">
         <BannerDot id="item1" />
         <BannerDot id="item2" />
