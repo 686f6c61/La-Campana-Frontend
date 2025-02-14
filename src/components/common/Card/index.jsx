a847cd572f14681e33c0ad2caf1ed44c37a8dead
Antonella Quintana <antonellaq25@gmail.com>
2025-02-14 16:10:02 -0300

    change logo

--- src/components/common/Card/index.jsx
+++ src/components/common/Card/index.jsx
@@ -4,12 +4,12 @@ const Card = ({ product }) => {
     return (
       <div className="relative w-full  rounded-b-[25px] bg-gray-100">
       <div className="relative">
-        <img src={product.image} alt={product.name} className="p-[6px] w-full h-[227px]" />
+        <img src={product.image} alt={product.title} className="p-[6px] w-full h-[227px]" />
         <span className="absolute top-2 left-2 bg-gray-200 font-open-sans text-black text-sm px-2 py-1 rounded">
           {product.discount}
         </span>
       </div>
-      <h2 className="text-sm font-bold mt-2">{product.name}</h2>
+      <h2 className="text-sm font-bold mt-2">{product.title}</h2>
       <p className="text-lg text-gray-700 mt-1">{product.price}</p>
       <p className="text-xs text-gray-500">IVA incluido</p>
     </div>
