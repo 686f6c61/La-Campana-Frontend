498b8d35e7cedde6b8a6f0065384d5d79f7b483b
AkiraYago <the.programmer.yago@gmail.com>
2025-03-05 09:47:30 -0300

    feature: add hover scale transition in topbar components

--- src/components/layouts/SocialDropdown.jsx
+++ src/components/layouts/SocialDropdown.jsx
@@ -4,7 +4,7 @@ import { IoIosArrowDown } from "react-icons/io";
 const SocialDropdown = ({ socialMedia = [] }) => {
   return (
     <div className="dropdown dropdown-end">
-      <button tabIndex={0} role="button" className="font-open-sans border-box h-8 w-fit rounded-2xl rounded-br-none flex items-center gap-2 bg-lacampana-gray4 px-4 py-1 text-lacampana-gray1 hover:bg-lacampana-gray1 hover:text-white transition duration-300 ease-in-out">
+      <button tabIndex={0} role="button" className="font-open-sans border-box h-8 w-fit rounded-2xl rounded-br-none flex items-center gap-2 bg-lacampana-gray4 px-4 py-1 text-lacampana-gray1 hover:scale-90 hover:bg-lacampana-gray1 hover:text-white transition duration-300 ease-in-out">
         Síguenos en<IoIosArrowDown />
       </button>
       <ul tabIndex={0} className="dropdown-content flex items-center bg-lacampana-white rounded-tr-none rounded-box z-1 w-52 p-2 shadow-sm z-10">
--- src/layout/Topbar/index.jsx
+++ src/layout/Topbar/index.jsx
@@ -20,7 +20,7 @@ const Topbar = () => {
             citySelected={citySelected}
             onCityChange={handleCityChange}
           />
-          <Link to="/tienda" className="lacampana-btn flex items-center justify-center border-box h-8 w-32 bg-lacampana-gray1 text-white hover:bg-lacampana-gray4 hover:text-lacampana-gray1 hover:border border-lacampana-gray1 transition duration-300 ease-in-out px-4 py-1">
+          <Link to="/tienda" className="lacampana-btn flex items-center justify-center border-box h-8 w-32 bg-lacampana-gray1 text-white hover:scale-90 hover:bg-lacampana-gray4 hover:text-lacampana-gray1 hover:border border-lacampana-gray1 transition duration-300 ease-in-out px-4 py-1">
             <p className="text-p3-desktop">Comprar ahora</p>
           </Link>
           <SocialDropdown 
