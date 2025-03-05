9f3eca264f64d38dbfe876f030d55237ef0917cc
AkiraYago <the.programmer.yago@gmail.com>
2025-03-05 09:41:41 -0300

    feature: add hover styles in topbar components

--- src/layout/Topbar/index.jsx
+++ src/layout/Topbar/index.jsx
@@ -1,18 +1,31 @@
+import { useState } from "react"
+import CitySelector from "../../components/layouts/CitySelector"
+import cities from "../../utils/cities"
+import SocialDropdown from "../../components/layouts/SocialDropdown"
+import socialMedia from "../../utils/socialMedia"
+import { Link } from "react-router"
+
 const Topbar = () => {
+  const [citySelected, setCitySelected] = useState(cities[0].name)
+
+  const handleCityChange = (city) => setCitySelected(city)
+
   return (
     <article>
-      <div className="max-w-screen-desktop flex flex-col gap-2 tablet:flex-row justify-self-center justify-between w-full tablet:py-2 tablet:px-2 text-p3-desktop">
+      <div className="max-w-screen-desktop flex flex-col tablet:items-center gap-2 tablet:flex-row justify-self-center justify-between w-full tablet:py-2 tablet:px-2 text-p3-desktop">
         <p className="text-p3-desktop text-start">ENVÍOS GRATIS</p>
         <section className="flex gap-2 tablet:gap-4 flex-wrap">
-          <button className="lacampana-btn bg-lacampana-gray4 px-4 py-1">
-            <p className="text-p3-desktop text-lacampana-gray1">Bogotá</p>
-          </button>
-          <button className="lacampana-btn bg-lacampana-gray1 px-4 py-1">
-            <p className="text-p3-desktop text-lacampana-white">Comprar ahora</p>
-          </button>
-          <button className="lacampana-btn bg-lacampana-gray4 px-4 py-1">
-            <p className="text-p3-desktop text-lacampana-gray1">Siguenos en</p>
-          </button>
+          <CitySelector
+            citiesList={cities}
+            citySelected={citySelected}
+            onCityChange={handleCityChange}
+          />
+          <Link to="/tienda" className="lacampana-btn flex items-center justify-center border-box h-8 w-32 bg-lacampana-gray1 text-white hover:bg-lacampana-gray4 hover:text-lacampana-gray1 hover:border border-lacampana-gray1 transition duration-300 ease-in-out px-4 py-1">
+            <p className="text-p3-desktop">Comprar ahora</p>
+          </Link>
+          <SocialDropdown 
+            socialMedia={socialMedia}
+          />
         </section>
       </div>
     </article>
---
+++ src/utils/socialMedia.js
@@ -0,0 +1,40 @@
+const socialMedia = [
+  {
+    id: 1,
+    title: "YouTube",
+    link: "https://www.youtube.com/channel/UCSB_jR1084XFuvYcR4koPWQ?view_as=subscriber",
+    icon: "/svg/socialMedia/youtube.svg",
+  },
+  {
+    id: 2,
+    title: "Instagram",
+    link: "https://www.instagram.com/lacampana.co/",
+    icon: "/svg/socialMedia/instagram.svg",
+  },
+  {
+    id: 3,
+    title: "TikTok",
+    link: "https://www.tiktok.com/@lacampana_acero",
+    icon: "/svg/socialMedia/tiktok.svg",
+  },
+  {
+    id: 4,
+    title: "Facebook",
+    link: "https://www.facebook.com/lacampanacolombia/",
+    icon: "/svg/socialMedia/facebook.svg",
+  },
+  {
+    id: 5,
+    title: "X",
+    link: "https://x.com/lacampana_acero",
+    icon: "/svg/socialMedia/x.svg",
+  },
+  {
+    id: 6,
+    title: "LinkedIn",
+    link: "https://www.linkedin.com/company/la-campana-servicios-de-acero-s-a-/?originalSubdomain=co",
+    icon: "/svg/socialMedia/linkedin.svg",
+  },
+]
+
+export default socialMedia
