d992358ff3c56467da1cbe3d2c281c1db02b1bea
AkiraYago <the.programmer.yago@gmail.com>
2025-03-07 13:45:42 -0300

    feature: update categories section responsive and add action button

--- src/sections/Home/Categories.jsx
+++ src/sections/Home/Categories.jsx
@@ -1,10 +1,10 @@
-import { Link } from "react-router"
 import ImageCard from "../../components/common/ImageCard"
 import categories from "../../utils/categories"
+import ActionButton from "../../components/common/ActionButton"
 
 const Categories = () => {
   return (
-    <div className="flex flex-col items-center gap-8">
+    <div className="flex flex-col items-center gap-8 px-4">
       <h3>Categorias <span className="text-lacampana-red2">Destacadas</span></h3>
       <ul className="grid grid-cols-2 tablet:grid-cols-4 desktop:grid-cols-5 gap-4 max-w-screen-desktop">
         {categories.slice(0, 5).map(category =>
@@ -17,9 +17,10 @@ const Categories = () => {
           />
         )}
       </ul>
-      <Link to="/tienda" className="lacampana-btn px-8 py-2 bg-lacampana-red2 font-semibold text-white">
-        Ver más categorías
-      </Link>
+      <ActionButton 
+        link="/tienda"
+        text="Ver más categorías"
+      />
     </div>
   )
 }
