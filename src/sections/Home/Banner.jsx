6772d7bd55ff27ce90381c37f44e0f8c2333c583
AkiraYago <the.programmer.yago@gmail.com>
2025-03-07 13:30:36 -0300

    feature: fix banner ui issues

--- src/sections/Home/Banner.jsx
+++ src/sections/Home/Banner.jsx
@@ -3,10 +3,11 @@ import BannerSlide from "../../components/Home/BannerSlide";
 
 const Banner = () => {
   return (
-    <div className="h-svh px-4 relative">
+    <div className="h-[85svh] tablet:h-[65svh] desktop:h-[75svh] px-4 relative">
       <div className="carousel w-full h-full">
         <BannerSlide id="item1" imgURL="/images/home/banner/banner.jpg" />
         <BannerSlide id="item2" imgURL="/images/home/banner/banner2.jpeg" />
+        <BannerSlide id="item3" imgURL="/images/home/banner/banner.jpg" />
       </div>
       <BannerCallToAction />
     </div>
