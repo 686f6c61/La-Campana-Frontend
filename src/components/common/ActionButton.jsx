b4a2d853a6c0ae98936daa16e28eecac7223f267
Yesika <yesikapr@gmail.com>
2025-03-27 12:30:15 -0300

    fixing pages

--- src/components/common/ActionButton.jsx
+++ src/components/common/ActionButton.jsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router"
+import { Link } from "react-router-dom";
+
 
 const ActionButton = ({
   text,
@@ -6,16 +7,28 @@ const ActionButton = ({
   bgColor = "lacampana-red2",
   borderColor,
   width = "fit",
+  padding = "py-2 px-8",
+  margin = "mt-8",
+  font = "font-semibold",
   link,
-  onClick }) => {
+  onClick,
+}) => {
   return (
     <Link
       to={link}
       onClick={onClick}
-      className={`lacampana-btn hover:bg-${textColor} hover:text-${bgColor} hover:scale-90 border ${borderColor ? `border-${borderColor}` : `border-${bgColor}`} bg-${bgColor} text-${textColor} font-semibold w-${width} py-2 px-8 transition duration-300 ease-in-out`}>
+      className={`lacampana-btn hover:scale-90
+        hover:bg-${textColor} hover:text-${bgColor}
+        ${margin}
+        border ${borderColor ? `border-${borderColor}` : `border-${bgColor}`}
+        bg-${bgColor} text-${textColor}
+        ${font} w-${width} ${padding}
+        transition duration-300 ease-in-out`}
+    >
       {text}
     </Link>
-  )
-}
+  );
+};
 
-export default ActionButton
+
+export default ActionButton;
