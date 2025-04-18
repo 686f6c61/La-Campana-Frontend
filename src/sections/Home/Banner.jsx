603b4346f9e85dce5cbf4cbd9b26359a1c87ab1e
Antonella Quintana <antonellaq25@gmail.com>
2025-04-18 10:23:37 -0300

    fix seo

--- src/sections/Home/Banner.jsx
+++ src/sections/Home/Banner.jsx
@@ -21,9 +21,9 @@ const Banner = () => {
   return (
     <div className="h-[85svh] tablet:h-[65svh] desktop:h-[75svh] p-3 tablet:p-0 relative">
       <div className="carousel w-full h-full">
-        <BannerSlide id="item1" imgURL="/images/home/banner/banner.jpg" />
-        <BannerSlide id="item2" imgURL="/images/home/banner/banner2.jpeg" />
-        <BannerSlide id="item3" imgURL="/images/home/banner/banner.jpg" />
+        <BannerSlide id="item1" imgURL="/images/home/banner/banner.jpg" alt="banner" />
+        <BannerSlide id="item2" imgURL="/images/home/banner/banner2.jpeg" alt="banner" />
+        <BannerSlide id="item3" imgURL="/images/home/banner/banner.jpg" alt="banner"/>
       </div>
       <BannerCallToAction
         currentSlide={currentSlide}
