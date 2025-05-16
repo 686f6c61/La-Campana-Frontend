11bcbcfcf05941a16a082f766f24adf58bdc600f
Antonella Quintana <antonellaq25@gmail.com>
2025-05-15 22:30:13 -0300

    fix button

--- src/sections/Blog/BlogsGallery.jsx
+++ src/sections/Blog/BlogsGallery.jsx
@@ -150,7 +150,7 @@ const BlogsGallery = () => {
         <button
           disabled={MAX_NUM_RENDERS * numRenders >= blogs?.length}
           onClick={handleLoadMore}
-          className="lacampana-btn mt-10 bg-lacampana-red2 px-8 py-2 mb-5 text-white disabled:bg-lacampana-gray2"
+          className="lacampana-btn mt-10 bg-lacampana-red2 px-8 py-2 mb-5 text-white disabled:bg-lacampana-gray2 md:pb-2 transition duration-300 ease-in-out hover:scale-90"
         >
           Cargar más
         </button>
